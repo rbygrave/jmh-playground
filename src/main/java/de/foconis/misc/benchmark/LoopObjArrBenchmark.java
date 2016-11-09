ebff2fd1cbcdd382ded8d989bf4987f1c4226a22
Rob Bygrave <robin.bygrave@gmail.com>
2016-11-09 13:13:17 +1300

    Remove other benchmark results from javadoc

--- src/main/java/de/foconis/misc/benchmark/LoopObjArrBenchmark.java
+++ src/main/java/de/foconis/misc/benchmark/LoopObjArrBenchmark.java
@@ -43,25 +43,8 @@ public class LoopObjArrBenchmark {
      * Java HotSpot(TM) 64-Bit Server VM (build 25.101-b13, mixed mode)
      *
      * $ mvn clean install
-     * $ java -cp target/benchmarks.jar de.foconis.misc.benchmark.LoopBenchmark
+     * $ java -cp target/benchmarks.jar de.foconis.misc.benchmark.LoopObjArrBenchmark
      *
-     * First run
-     * Benchmark                                               Mode  Cnt       Score       Error  Units
-     * LoopBenchmark.testBasicForLoop                thrpt    5  144337,290 ±  8449,253  ops/s
-     * LoopBenchmark.testForEach                     thrpt    5  131612,294 ± 38038,652  ops/s
-     * LoopBenchmark.testForEachLambda               thrpt    5  134350,796 ± 19255,047  ops/s
-     * LoopBenchmark.testForLoopWithCachedFinalSize  thrpt    5  148162,065 ± 16794,222  ops/s
-     * LoopBenchmark.testForLoopWithCachedSize       thrpt    5  151834,460 ±  5949,224  ops/s
-     * LoopBenchmark.testWhileIerator                thrpt    5  138113,430 ± 35968,835  ops/s
-     *
-     * Second run
-     * Benchmark                                      Mode  Cnt       Score       Error  Units
-     * LoopBenchmark.testBasicForLoop                thrpt    5  145287,696 ±  3022,676  ops/s
-     * LoopBenchmark.testForEach                     thrpt    5  135458,414 ± 11743,290  ops/s
-     * LoopBenchmark.testForEachLambda               thrpt    5  136406,003 ± 10512,011  ops/s
-     * LoopBenchmark.testForLoopWithCachedFinalSize  thrpt    5  148242,055 ± 34191,601  ops/s
-     * LoopBenchmark.testForLoopWithCachedSize       thrpt    5  149890,514 ± 16730,079  ops/s
-     * LoopBenchmark.testWhileIerator                thrpt    5  143421,289 ± 22595,459  ops/s
      * </pre>
      *
      * @param args
